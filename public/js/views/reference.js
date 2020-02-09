6b39fddb7fa0d1f275a87e4894699f66af9141f1
Sabina <snow.sab@yahoo.com>
2020-02-10 01:45:47 +0300

    change4.

--- public/js/views/reference.js
+++ public/js/views/reference.js
@@ -24,7 +24,23 @@ webix.protoUI({
 
 }, webix.ui.view);
 
-
+// webix.ready(function(){
+//             grida = webix.ui({
+//                 container:"testA",
+//                 view:"datatable",
+//                 columns:[
+//                     { id:"rank",    header:"", css:"rank",          width:50},
+//                     { id:"title",   header:"Film title",width:200},
+//                     { id:"year",    header:"Released" , width:80},
+//                     { id:"votes",   header:"Votes",     width:100}
+//                 ],
+//                 autoheight:true,
+//                 autowidth:true,
+//                 data: [
+//                     { id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
+//                     { id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2}
+//                 ]
+//             });
 
 // dataTable: function() {
 //     webix.ui({
